d109af79282fa0aacf09b2c77a293b929f1552ec
sumit-152 <sumitsingh.bits@aol.com>
2017-07-26 00:19:14 +0200

    Coursera Assignment 2
    Changed Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Below two functions are created as part of coursera assignment 2.
+## Main goal is to check and understand about lexical scoping. 
+## makeCacheMatrix :This function retrieve inverse of the matrix 
+## from caches rather than calculating each time.This will save the time-
+## consuming computations.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL    ## Initialization of Inv as null to hold inverse matrix
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    get <- function() x ## define the get fucntion to return matrix value
+    
+    setInverse <- function(inverse) inv <<- inverse 
+                                ## assigns value of inv in parent environment
+    getInverse <- function() inv
+                                ## gets the value of inv where called
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
+## cacheSolve: This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
+    
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if (!is.null(inv)) {   ## Check the cached data for Inv 
+        message("getting cached data")
+        return(inv)        ## Return the Inv from cached 
+    }
+    data <- x$get()
+    inv <- solve(data, ...) ## Function call to matrix inverse
+    x$setInverse(inv)
+    inv
 }
